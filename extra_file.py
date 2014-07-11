41ba1f0237b36756171b99649c8c82aa63a11e6f
guillermokrh <memo.hernandez182@gmail.com>
2014-07-11 17:53:29 -0400

    Add Python file, Update text_file

---
+++ extra_file.py
@@ -0,0 +1,11 @@
+#Author: Guillermo Rojas
+# Creates short python program to practice staging and committing with GitHub
+# July 11, 2014
+
+#Defines Functions
+def fun_with_booleans():
+	goal_scored = True
+	if goal_scored:
+		print "GOOOOOOOOOOOOOLLLLL" #prints gol if a goal is scored
+#Calls Function
+fun_with_booleans()
