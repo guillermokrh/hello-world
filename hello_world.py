2725f3fce3443b7c11533561cf221a06f971c865
guillermokrh <memo.hernandez182@gmail.com>
2014-07-11 12:46:08 -0400

    Create hello_world.py
    Create first hello world file

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+# Hello World program in python
+# Autor: Guillermo Rojas
+
+def hello_world(): #function that defines hello world
+  print "Hello World!" #prints "hello world!"
+  
+hello_world() # runs hello world function
