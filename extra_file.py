c062851ebc717cfdfce028e31e3df11d85a2c4d6
guillermokrh <memo.hernandez182@gmail.com>
2014-07-11 18:37:23 -0400

    Stage the latest version of extra_file.py

--- extra_file.py
+++ extra_file.py
@@ -5,6 +5,7 @@
 #Defines Functions
 def fun_with_booleans():
 	goal_scored = True
+	year_of_current_world_cup = 2014
 	if goal_scored:
 		print "GOOOOOOOOOOOOOLLLLL" #prints gol if a goal is scored
 
@@ -14,3 +15,4 @@ def fun_with_booleans():
 fun_with_booleans() 
 fun_with_booleans() #going to call function twice just so I have something
                     # that I can stage
+
