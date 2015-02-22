fd91ab7deaeafc6853d85241df72ddcb4d937ad2
guillermokrh <guillermo.krh@gmail.com>
2015-02-21 20:14:45 -0600

    add the_heavenly_option.c

---
+++ the_heavenly_option.c
@@ -0,0 +1,14 @@
+/**
+* author: guillermokrh
+* Description: prints out text
+*/
+
+#include <stdio.h>
+
+int main() {
+
+	printf("C is the Heavenly Option.\n");
+	printf("By: Los Campesinos!\n");
+
+	return 0;
+}
